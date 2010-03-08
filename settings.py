b42e1c9085c0c470e3a13bd5162b07284cc93892
Jared Forsyth <jared@jaredforsyth.com>
2010-03-07 19:17:35 -0700

    initial commit

---
+++ settings.py
@@ -0,0 +1,116 @@
+# Django settings for jfcom project.
+
+DEBUG = True
+TEMPLATE_DEBUG = DEBUG
+
+ADMINS = (
+    ('Jared Forsyth', 'jared@byu.edu'),
+)
+
+MANAGERS = ADMINS
+
+DATABASE_ENGINE = 'sqlite3'           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+DATABASE_NAME = 'my.db'             # Or path to database file if using sqlite3.
+DATABASE_USER = ''             # Not used with sqlite3.
+DATABASE_PASSWORD = ''         # Not used with sqlite3.
+DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
+DATABASE_PORT = ''             # Set to empty string for default. Not used with sqlite3.
+
+# Local time zone for this installation. Choices can be found here:
+# http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
+# although not all choices may be available on all operating systems.
+# If running in a Windows environment this must be set to the same as your
+# system time zone.
+TIME_ZONE = 'America/Chicago'
+
+# Language code for this installation. All choices can be found here:
+# http://www.i18nguy.com/unicode/language-identifiers.html
+LANGUAGE_CODE = 'en-us'
+
+SITE_ID = 1
+
+# If you set this to False, Django will make some optimizations so as not
+# to load the internationalization machinery.
+USE_I18N = True
+
+import os
+# Absolute path to the directory that holds media.
+# Example: "/home/media/media.lawrence.com/"
+MEDIA_ROOT = os.path.join(os.path.dirname(__file__), 'media')
+
+# URL that handles the media served from MEDIA_ROOT. Make sure to use a
+# trailing slash if there is a path component (optional in other cases).
+# Examples: "http://media.lawrence.com", "http://example.com/media/"
+MEDIA_URL = '/media/'
+
+# URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
+# trailing slash.
+# Examples: "http://foo.com/media/", "/media/".
+ADMIN_MEDIA_PREFIX = '/admin_media/'
+
+# Make this unique, and don't share it with anybody.
+SECRET_KEY = 'ix*p@k-d&26+@8aq)&0l*=&h38^&puc54bx%b!m!_#4nh)v5bo'
+
+# List of callables that know how to import templates from various sources.
+TEMPLATE_LOADERS = (
+    'django.template.loaders.filesystem.load_template_source',
+    'django.template.loaders.app_directories.load_template_source',
+#     'django.template.loaders.eggs.load_template_source',
+)
+
+MIDDLEWARE_CLASSES = (
+    'django.contrib.flatpages.middleware.FlatpageFallbackMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+)
+
+ROOT_URLCONF = 'jfcom.urls'
+
+import os
+TEMPLATE_DIRS = (
+    os.path.join(os.path.dirname(__file__),'templates'),        
+    # os.path.join(os.path.dirname(__file__), 'templates', 'todo', 'templates', 'todo'),        
+    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
+    # Always use forward slashes, even on Windows.
+    # Don't forget to use absolute paths, not relative paths.
+)
+TEMPLATE_CONTEXT_PROCESSORS = (
+    "django.core.context_processors.auth",
+    "django.core.context_processors.debug",
+    "django.core.context_processors.i18n",
+    "django.core.context_processors.media",
+    "django.core.context_processors.request",
+    "navbar.context_processors.crumbs",
+    "navbar.context_processors.navbar",
+    "navbar.context_processors.navtree",
+    "navbar.context_processors.navbars",
+)
+#admin/        comments/     databrowse/   gis/          __init__.pyc  redirects/    sites/        
+#admindocs/    contenttypes/ flatpages/    humanize/     localflavor/  sessions/     syndication/  
+#auth/         csrf/         formtools/    __init__.py   markup/       sitemaps/     webdesign/    
+
+
+INSTALLED_APPS = (
+    'django.contrib.auth',
+    'django.contrib.admin',
+    'django.contrib.admindocs',
+    'django.contrib.comments',
+    'django.contrib.contenttypes',
+    'django.contrib.flatpages',
+    'django.contrib.markup',
+    'django.contrib.sessions',
+    'django.contrib.sites',
+    'django.contrib.syndication',
+    'django.contrib.webdesign',
+
+    'basic.blog',
+    'navbar',
+    'tracker',
+    'tagging',
+    'todo',
+    # mine
+    'appsettings',
+    'feedback',
+    'myprojects',
+)
