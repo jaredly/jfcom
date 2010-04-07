caceb796a63e8cd8c19b944c36231136b270d463
Jared Forsyth <jabapyth@gmail.com>
2010-04-07 09:34:54 -0600

    fixing reversion integration

--- admin.py
+++ admin.py
@@ -3,20 +3,25 @@
 # activate reversion
 
 from django.contrib import admin
-from reversion.admin import ReversionAdmin
+from reversion.admin import VersionAdmin
 from basic.blog.models import Post
 
 class PostRevAdmin(VersionAdmin):
     """Admin settings"""
 
-admin.site.register(Post, PostRevAdmin)
+#admin.site.register(Post, PostRevAdmin)
 
 from myprojects.models import Project
 
 class ProjectRevAdmin(VersionAdmin):
     pass
 
-admin.site.register(Project, ProjectRevAdmin)
+#admin.site.register(Project, ProjectRevAdmin)
+import reversion
+reversion.register(Post)
 
+from reversion.models import Revision, Version
+admin.site.register(Revision)
+admin.site.register(Version)
 
 # vim: et sw=4 sts=4
